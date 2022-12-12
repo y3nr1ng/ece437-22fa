f01bac1449232963ad61e76105d9e6ad33925cda
Yen-Ting Liu <ytliu2@illinois.edu>
2022-12-12 00:11:50 -0600

    Allow environment variable to determine FP API

--- Python/ece437/src/ece437/__init__.py
+++ Python/ece437/src/ece437/__init__.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 # attempt to discover fp api
-OKFP_ROOT = 'C:/Program Files/Opal Kelly/FrontPanelUSB'
+OKFP_ROOT_DEFAULT = 'C:/Program Files/Opal Kelly/FrontPanelUSB'
+OKFP_ROOT = os.environ.get('OKFP_ROOT', OKFP_ROOT_DEFAULT)
 OKFP_PYTHON_ROOT = OKFP_ROOT + '/API/Python/3.6/x64'
 if not os.path.exists(OKFP_PYTHON_ROOT):
     raise RuntimeError(f'unable to find FrontPanel API')
