d1552f99b7b349fce995b6a07a49308c2d0bfa56
Yen-Ting Liu <ytliu2@illinois.edu>
2022-12-13 17:46:54 -0600

    Add device detection and firmware upload bypass

--- Python/ece437/src/ece437/ok.py
+++ Python/ece437/src/ece437/ok.py
@@ -1,69 +1,83 @@
 import logging
 import os
+from typing import Tuple
 
 import ok
 
 logger = logging.getLogger(__name__)
 
+
 class OKFrontPanel:
-    def __init__(self, serial='', firmware_path = None):
+    def __init__(self, serial="", firmware_path=None):
         self._device = None
         self._serial = serial
         self._firmware_path = firmware_path
-        
+
         self._device_info = None
-        
+
     def __enter__(self):
         self._device = ok.okCFrontPanel()
         self.open()
-        
+
         return self
-    
+
     def __exit__(self, *exc_args):
         self.close()
-        
+
         del self._device
         self._device = None
-    
+
     @property
     def device_id(self) -> str:
         """Device ID string."""
         return self._device_info.deviceID
-    
+
     @property
     def serial_number(self) -> str:
         """Device serial number."""
         return self._device_info.serialNumber
-    
+
+    def enumerate(self) -> Tuple[str]:
+        """Return serial number of available devices."""
+        devices = ok.okCFrontPanelDevices()
+        n_devices = devices.GetCount()
+        if n_devices < 1:
+            raise RuntimeError("unable to find OK device")
+        serials = tuple(devices.GetSerial(index) for index in range(n_devices))
+        return serials
+
     def open(self):
         if self._device is None:
             raise RuntimeError("please wrap the class with context statement")
+        
+        self.enumerate()
         status = self._device.OpenBySerial(self._serial)
         self.parse_error_code(status)
-        
+
         if self._firmware_path is not None:
             if not os.path.exists(self._firmware_path):
-                raise ValueError(f'firmware file does not exist')
-            logger.info(f"upload firmware '{self._firmware_path}'")
-            self._device.ConfigureFPGA(self._firmware_path)
-        
+                logger.error(f"firmware file does not exist")
+            else:
+                logger.info(f"upload firmware '{self._firmware_path}'")
+                self._device.ConfigureFPGA(self._firmware_path)
+
         device_info = ok.okTDeviceInfo()
         status = self._device.GetDeviceInfo(device_info)
         self.parse_error_code(status)
         self._device_info = device_info
-        
+
     def close(self):
         self._device.Close()
         self._device_info = None
-    
+
     @staticmethod
     def parse_error_code(code):
         lut = {
-            0: (None, 'success'),
-            -1: (RuntimeError, 'non-descriptive failure'),
-            -2: (TimeoutError, 'transfer timed out'),
+            0: (None, "success"),
+            -1: (RuntimeError, "non-descriptive failure"),
+            -2: (TimeoutError, "transfer timed out"),
             # TODO need to add reset of the errors
         }
-        klass, message = lut.get(code, (RuntimeError, f'unknown error code ({code})'))
+        klass, message = lut.get(code, (RuntimeError, f"unknown error code ({code})"))
         if klass:
             raise klass(message)
